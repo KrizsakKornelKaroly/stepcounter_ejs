77d300953128ae58fa754e83c0366118292831eb
KrizsakKornelKaroly <krizsakkornelkaroly@turr.hu>
2025-12-18 21:07:00 +0100

    standard ejs setup

---
+++ modules/core.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+//const db = require('./db');
+const ejs = require('ejs');
+
+router.get('/', (req, res) => {
+    ejs.renderFile('views/system/index.ejs', { user: req.session.user }, (err, html) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send(html);
+    });
+});
+
+module.exports = router;    
