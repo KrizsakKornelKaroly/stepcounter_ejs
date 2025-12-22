8fcf6d20af15b659acc3ea7cbd2806d1811fe5dc
KrizsakKornelKaroly <krizsakkornelkaroly@turr.hu>
2025-12-22 21:03:01 +0100

    critical error fixed, step addition + removal impl.

--- modules/steps.js
+++ modules/steps.js
@@ -4,6 +4,8 @@ const ejs = require('ejs');
 const db = require('./db');
 const moment = require('moment');
 
+// Dashboard route
+
 router.get('/', loginCheck, (req, res) => {
     db.query('SELECT * FROM statistics WHERE uId = ?', [req.session.user.id], (err, results) => {
         if (err) {
@@ -14,14 +16,11 @@ router.get('/', loginCheck, (req, res) => {
         }
 
 
-        statistics = results[0];
+        const statistics = results[0] || {};
 
 
         if (statistics.maxStepDate) {
             statistics.maxStepDate = moment(statistics.maxStepDate).format('YYYY-MM-DD');
-        }
-        else{
-            statistics.maxStepDate = 'N/A';
         }
 
         ejs.renderFile('views/steps/dashboard.ejs', { session: req.session, moment: moment, statistics: statistics }, (err, html) => {
@@ -40,7 +39,7 @@ router.get('/', loginCheck, (req, res) => {
 
 });
 
-router.get('/steps', loginCheck, (req, res) => {    
+router.get('/steps', loginCheck, (req, res) => {
     db.query('SELECT * FROM steps WHERE user_id = ? ORDER BY date DESC', [req.session.user.id], (err, results) => {
         if (err) {
             console.log(err);
@@ -48,9 +47,6 @@ router.get('/steps', loginCheck, (req, res) => {
             req.session.severity = 'danger';
             return res.redirect('/steps');
         }
-
-
-
 
         stepList = results;
 
@@ -69,6 +65,111 @@ router.get('/steps', loginCheck, (req, res) => {
 
 });
 
+// New step entry 
+
+router.get('/new', loginCheck, (req, res) => {
+    ejs.renderFile('views/steps/steps-new.ejs', { session: req.session, moment: moment }, (err, html) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        req.session.error = '';
+        req.session.body = undefined;
+        res.send(html);
+    });
+});
+
+router.post('/new', loginCheck, (req, res) => {
+    const { date = moment().format('YYYY-MM-DD'), steps } = req.body;
+
+    req.session.body = req.body;
+
+    if (!date || !steps) {
+        req.session.error = 'Kérlek, töltsd ki az összes mezőt!';
+        req.session.severity = 'warning';
+        return res.redirect('/steps/new');
+    }
+
+    db.query('SELECT date FROM steps WHERE user_id = ? ', [req.session.user.id], (err, results) => {
+        if (err) {
+            console.log(err);
+            req.session.error = 'Hiba történt az adatbázis lekérdezése során.';
+            req.session.severity = 'danger';
+            return res.redirect('/steps/new');
+        }
+        const existingDates = results.map(row => moment(row.date).format('YYYY-MM-DD'));
+
+        if (existingDates.includes(date)) {
+            req.session.error = 'Erre a dátumra már rögzítettél lépésadatot!';
+            req.session.severity = 'warning';
+            return res.redirect('/steps/new');
+        }
+    });
+
+    db.query('INSERT INTO steps (user_id, date, steps) VALUES (?, ?, ?)', [req.session.user.id, date, steps], (err, result) => {
+        if (err) {
+            console.log(err);
+            req.session.error = 'Hiba történt az adatbázis lekérdezése során.';
+            req.session.severity = 'danger';
+            return res.redirect('/steps/new');
+        }
+        req.session.error = 'Lépésadat sikeresen hozzáadva!';
+        req.session.severity = 'success';
+        req.session.body = undefined;
+        return res.redirect('/steps/steps');
+    });
+});
+
+// Delete step entry
+// SELECT + DELETE user_id feltétel: csak a saját lépésadatokat lehessen törölni és lekérdezni 
+
+router.get('/delete/:id', loginCheck, (req, res) => {
+    const stepId = req.params.id;
+
+    db.query('SELECT * FROM steps WHERE id = ? AND user_id = ?', [stepId, req.session.user.id], (err, results) => {
+        if (err) {
+            console.log(err);
+            req.session.error = 'Hiba történt az adatbázis lekérdezése során.';
+            req.session.severity = 'danger';
+            return res.redirect('/steps');
+        }
+
+        if (results.length == 0) {
+            req.session.error = 'A megadott lépésadat nem található.';
+            req.session.severity = 'warning';
+            return res.redirect('/steps');
+        }
+
+        let stepItem = results[0];
+        stepItem.date = moment(stepItem.date).format('YYYY-MM-DD');
+
+        ejs.renderFile('views/steps/steps-delete.ejs', { session: req.session, stepItem: stepItem }, (err, html) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            res.send(html);
+        });
+    });
+});
+
+router.post('/delete/:id', loginCheck, (req, res) => {
+    const stepId = req.params.id;
+    db.query('DELETE FROM steps WHERE id = ? AND user_id = ?', [stepId, req.session.user.id], (err, result) => {
+        if (err) {
+            console.log(err);
+            req.session.error = 'Hiba történt az adatbázis lekérdezése során.';
+            req.session.severity = 'danger';
+            return res.redirect('/steps');
+        }
+        req.session.error = 'Lépésadat sikeresen törölve!';
+        req.session.severity = 'success';
+        return res.redirect('/steps/steps');
+    });
+});
+
+
+// loginCheck middleware
 
 function loginCheck(req, res, next) {
     if (req.session.user) {
